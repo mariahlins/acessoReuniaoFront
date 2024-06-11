a7ecd2fcd1d57ea034aacbccd3716e9e4b406f44
Gustavo Santos <alexandregustavo00@gmail.com>
2024-06-11 14:50:52 -0300

    refactor: deixa o arquivo de conxeão mais compacto

--- public/js/conexaoAPI/conexao.js
+++ public/js/conexaoAPI/conexao.js
@@ -4,36 +4,15 @@ const SalaController = require('./APIendpoints/sala.js');
 const UsuarioController = require('./APIendpoints/usuario.js');
 
 class Controller{
-
-    static async fazerLogin(){
-        RecepcionistaController.fazerLogin();
-    }
-    
-    static async listarReservas(){
-        ReservaController.listarReservas();
-    }
-
-    static async listarSalasDropdown(){
-        SalaController.listarSalasDropdown();
-    }
-
-    static async mostrarSalasTerreo(){
-        SalaController.mostrarSalasTerreo();
-    }
-
-    static async listarReservasHoje(){
-        ReservaController.listarReservasHoje();
-    }
-
-    static async listarUsuarios(){
-        UsuarioController.listarUsuarios();
-    }
-
-    static async listarSalas(){
-        SalaController.listarSalas();
-    }
-
+    static async fazerLogin() { RecepcionistaController.fazerLogin(); }
+    static async listarReservas() { ReservaController.listarReservas(); }
+    static async listarSalasDropdown() { SalaController.listarSalasDropdown(); }
+    static async mostrarSalasTerreo() { SalaController.mostrarSalasTerreo(); }
+    static async listarReservasHoje() { ReservaController.listarReservasHoje(); }
+    static async listarUsuarios() { UsuarioController.listarUsuarios(); }
+    static async listarSalas() { SalaController.listarSalas(); }
 }
+
 document.addEventListener('DOMContentLoaded', () => {
     Controller.fazerLogin();
     Controller.listarReservas();
