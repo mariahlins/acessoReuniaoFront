0fb44aa873a078fb9701dbfd7d041bb8fd977e53
David Enéas <70227448+EneasDavid@users.noreply.github.com>
2024-06-12 20:07:58 -0300

    Correção de login e conexão com o back

--- public/js/conexaoAPI/APIendpoints/recepcionista.js
+++ public/js/conexaoAPI/APIendpoints/recepcionista.js
@@ -1,19 +1,24 @@
-class RecepcionistaController{
+import { fazerLogin as _fazerLogin } from './APIendpoints/recepcionista.js';
 
-    static async fazerLogin(){
-        document.getElementById('login-form').addEventListener('submit', async(event) => {
+class RecepcionistaController {
+    static async fazerLogin() {
+        document.getElementById('login-form').addEventListener('submit', async (event) => {
             event.preventDefault();
             const formData = new FormData(document.getElementById('login-form'));
-            const data = { login : formData.get('login'), senha : formData.get('senha') }; 
-            try{
+            const data = { login: formData.get('login'), senha: formData.get('senha') };
+            console.log(data);
+            try {
                 await axios.post(`http://localhost:3000/recepcionista/login`, data);
-                //colocar aqui o que fazer após o login
-            }catch(error){
+                // coloque aqui o que fazer após o login
+            } catch (error) {
                 console.error('Erro ao fazer login', error);
             }
         });
     }
-
-
 }
-module.exports = RecepcionistaController;
+
+document.addEventListener('DOMContentLoaded', () => {
+    RecepcionistaController.fazerLogin();
+});
+
+export default RecepcionistaController;
