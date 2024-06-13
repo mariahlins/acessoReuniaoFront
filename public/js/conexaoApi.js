7d4794d7e1a9737e2a06aac7e069cac6d0437334
Gustavo Santos <alexandregustavo00@gmail.com>
2024-06-13 18:01:37 -0300

    fix: ajusta a integração com o login para funcionar corretamente

--- public/js/conexaoApi.js
+++ public/js/conexaoApi.js
@@ -1,26 +1,27 @@
-class RecepcionistaController {
-    static async fazerLogin(login, senha) {
+
+class Controller{
+
+    static async fazerLogin() {
         document.getElementById('login-form').addEventListener('submit', async (event) => {
             event.preventDefault(); 
-            console.log("AQUI UM");
             const formData = new FormData(document.getElementById('login-form'));
             const data={
                 login:formData.get('login'),
                 senha:formData.get('senha'),
             };
-            console.log(data);
-            /*try {
+            try {
                 const response = await axios.post(`http://localhost:3000/recepcionista/login`, data);
-                // Coloque aqui o que fazer após o login bem-sucedido
                 console.log('Login bem-sucedido:', response);
+                window.location.href = './public/views/afterLogin/home.html';
             } catch (error) {
                 console.error('Erro ao fazer login:', error);
                 throw error;
-            }*/
+            }
         });
     }
+
 }
-console.log("AQUI PORRA");
+
 document.addEventListener('DOMContentLoaded', () => {
-    RecepcionistaController.fazerLogin();
+    Controller.fazerLogin();
 });
--- public/js/helpers/functions.js
+++ public/js/helpers/functions.js
@@ -1,20 +1,12 @@
 const moment = require('moment');
 
-function ocultarDocumento(documento) {
+export function ocultarDocumento(documento){
     const inicio = documento.slice(0, 3);
     const fim = documento.slice(-2);
     const asteriscos = '*'.repeat(documento.length - 5);
     return inicio + asteriscos + fim;
-}
+} 
 
-function formatarData(data){
+export function formatarData(data){
     return moment(data).tz('America/Sao_Paulo').format('DD/MM/YYYY');
 }
-
-function checkHoje(data) {
-    const hoje = formatarData(new Date());
-    const dataAlvo = formatarData(data);
-    return hoje === dataAlvo;
-}
-
-module.exports = { ocultarDocumento, formatarData, checkHoje }
