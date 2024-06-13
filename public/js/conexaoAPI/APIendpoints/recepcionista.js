9243faf9a645ab2b4a0c012ee159fc239547a1fe
David Enéas <70227448+EneasDavid@users.noreply.github.com>
2024-06-13 10:23:54 -0300

    Adição de login

--- public/js/conexaoAPI/APIendpoints/recepcionista.js
+++ public/js/conexaoAPI/APIendpoints/recepcionista.js
@@ -1,24 +1,26 @@
-import { fazerLogin as _fazerLogin } from './APIendpoints/recepcionista.js';
-
 class RecepcionistaController {
-    static async fazerLogin() {
+    static async fazerLogin(login, senha) {
         document.getElementById('login-form').addEventListener('submit', async (event) => {
-            event.preventDefault();
+            event.preventDefault(); 
+            console.log("AQUI UM");
             const formData = new FormData(document.getElementById('login-form'));
-            const data = { login: formData.get('login'), senha: formData.get('senha') };
+            const data={
+                login:formData.get('login'),
+                senha:formData.get('senha'),
+            };
             console.log(data);
-            try {
-                await axios.post(`http://localhost:3000/recepcionista/login`, data);
-                // coloque aqui o que fazer após o login
+            /*try {
+                const response = await axios.post(`http://localhost:3000/recepcionista/login`, data);
+                // Coloque aqui o que fazer após o login bem-sucedido
+                console.log('Login bem-sucedido:', response);
             } catch (error) {
-                console.error('Erro ao fazer login', error);
-            }
+                console.error('Erro ao fazer login:', error);
+                throw error;
+            }*/
         });
     }
 }
-
+console.log("AQUI PORRA");
 document.addEventListener('DOMContentLoaded', () => {
     RecepcionistaController.fazerLogin();
 });
-
-export default RecepcionistaController;
