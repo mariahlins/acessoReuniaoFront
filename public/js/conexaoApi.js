04e405987b1a89c6452fc17f5e5353cdb67cdcd5
Gustavo Santos <alexandregustavo00@gmail.com>
2024-06-13 18:45:09 -0300

    fix: resolve o alerta do console

--- public/js/conexaoApi.js
+++ public/js/conexaoApi.js
@@ -1,25 +1,25 @@
-
 class Controller{
 
     static async fazerLogin() {
-        document.getElementById('login-form').addEventListener('submit', async (event) => {
-            event.preventDefault(); 
-            const formData = new FormData(document.getElementById('login-form'));
-            const data={
-                login:formData.get('login'),
-                senha:formData.get('senha'),
-            };
-            try {
-                const response = await axios.post(`http://localhost:3000/recepcionista/login`, data);
-                console.log('Login bem-sucedido:', response);
-                window.location.href = './public/views/afterLogin/home.html';
-            } catch (error) {
-                console.error('Erro ao fazer login:', error);
-                throw error;
-            }
-        });
+        const loginForm = document.getElementById('login-form');
+        if (loginForm) {
+            loginForm.addEventListener('submit', async (event) => {
+                event.preventDefault(); 
+                const formData = new FormData(loginForm);
+                const data={
+                    login:formData.get('login'),
+                    senha:formData.get('senha'),
+                };
+                try {
+                    const response = await axios.post(`http://localhost:3000/recepcionista/login`, data);
+                    console.log('Login bem-sucedido:', response);
+                    window.location.href = './public/views/afterLogin/home.html';
+                } catch (error) {
+                    throw error;
+                }
+            });
+        }
     }
-
 }
 
 document.addEventListener('DOMContentLoaded', () => {
