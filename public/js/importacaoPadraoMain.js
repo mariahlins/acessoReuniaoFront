6315a8dbf18e5b50c8c2745b9b91c13585e0634b
mariahlins <mariahlinssena4@gmail.com>
2024-06-13 21:06:38 -0300

    Criação das 3 telas de consultas e mudanças no modal

--- public/js/importacaoPadraoMain.js
+++ public/js/importacaoPadraoMain.js
@@ -1,25 +1,55 @@
 $(function() {
-    // Importa o conteúdo do cabeçalho
-    $.get("/public/components/cabecalho.html", function(cabecalhoContent) {
-        // Adiciona o conteúdo do cabeçalho ao começo do main
-        $("main").append(cabecalhoContent); 
-        //importa o conteudo do modal do cpf
-        $.get("/public/components/modalcpf.html", function(modalcpfContent){
-            $("main").append(modalcpfContent);
-            //importa o conteudo do modal para reserva de empresas
-            $.get("/public/components/modalcnpj.html",function(modalcnpjContent){
-                $("main").append(modalcnpjContent);
-                // Importa o conteúdo do corpo após o cabeçalho ser carregado
-                $.get("/public/components/corpo.html", function(corpoContent) {
-                    // Adiciona o conteúdo do corpo após o cabeçalho dentro do main
-                    $("main").append(corpoContent); 
-                    // Importa o conteúdo do footer após o corpo ser carregado
-                    $.get("/public/components/footer.html", function(footerContent) {
-                        // Adiciona o conteúdo do footer após o corpo dentro do main
-                        $("main").append(footerContent);
-                    });
+    function loadPageComponents(title,subtitle){
+        const pagina = window.location.pathname.split("/").pop();
+        // Importa o conteúdo do cabeçalho
+        $.get("/public/components/cabecalho.html", function(cabecalhoContent) {
+            $("main").prepend(cabecalhoContent); 
+            //atualiza referenciando o id no titulo
+            $("#main-title").text(title);
+            $("#sub-title").text(subtitle);
+
+            //importa o conteudo do modal do cpf
+            $.get("/public/components/modalcpf.html", function(modalcpfContent){
+                $("main").append(modalcpfContent);
+
+                //importa o conteudo do modal para reserva de empresas
+                $.get("/public/components/modalcnpj.html",function(modalcnpjContent){
+                    $("main").append(modalcnpjContent);
+
+                    let url;
+                        switch (pagina) {
+                            case "home.html":
+                                url = "/public/components/corpo.html";
+                                break;
+                            case "reservas.html":
+                                url = "/public/components/corpoReservas.html";
+                                break;
+                            case "salas.html":
+                                url = "/public/components/corpoSalas.html";
+                                break;
+                            case "usuarios.html":
+                                url = "/public/components/corpoUsuarios.html";
+                                break;
+                            default:
+                                console.error("Página não encontrada: " + pagina);
+                                return; 
+                        }
+
+                        if (url) {
+                            $.get(url, function(content) {
+                                $("main").append(content); 
+                            });
+
+                            // Importa o conteúdo do footer após o corpo ser carregado
+                            //ta dentro da função pois preciso que o querido seja o último a ser carregado
+                            $.get("/public/components/footer.html", function(footerContent) {
+                                // Adiciona o conteúdo do footer após o corpo dentro do main
+                                $("main").append(footerContent);
+                            });
+                        }
                 });
             });
         });
-    });
+    }
+    loadPageComponents(window.pageTitle, window.pageSubtitle);
 });
