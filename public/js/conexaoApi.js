1e9c819992ef088476862bf8398a4a20ac5d7d6f
Gustavo Santos <alexandregustavo00@gmail.com>
2024-06-13 21:38:41 -0300

    fix: add o metodo de exibir a data atual na home

--- public/js/conexaoApi.js
+++ public/js/conexaoApi.js
@@ -1,4 +1,26 @@
+function formatarDataBr(data) {
+    const dia = data.getDate().toString().padStart(2, '0');
+    const mes = (data.getMonth() + 1).toString().padStart(2, '0'); 
+    const ano = data.getFullYear();
+    return `${dia}/${mes}/${ano}`;
+}
+
+function ocultarDocumento(documento) {
+    const inicio = documento.slice(0, 3);
+    const fim = documento.slice(-2);
+    const asteriscos = '*'.repeat(documento.length - 5);
+    return inicio + asteriscos + fim;
+}
+
 class Controller{
+
+    static async exibirDataAtual(){
+        const dataAtual = document.getElementById('data-hoje');
+        if(dataAtual){
+            const data = new Date();
+            dataAtual.innerText = formatarDataBr(data);
+        }
+    }
 
     static async fazerLogin() {
         const loginForm = document.getElementById('login-form');
@@ -12,7 +34,6 @@ class Controller{
                 };
                 try {
                     const response = await axios.post(`http://localhost:3000/recepcionista/login`, data);
-                    console.log('Login bem-sucedido:', response);
                     window.location.href = './public/views/afterLogin/home.html';
                 } catch (error) {
                     throw error;
@@ -23,5 +44,17 @@ class Controller{
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    Controller.fazerLogin();
+    // mutation observer para verificar se o elementos ja foram carregados
+    const observer = new MutationObserver((_, observer) => {
+        const dataHoje = document.getElementById('data-hoje');
+        const loginComponent = document.getElementById('login-component');
+        if (dataHoje) {
+            Controller.exibirDataAtual();
+            observer.disconnect();
+        }
+        if (loginComponent) {
+            Controller.fazerLogin();
+        }
+    });
+    observer.observe(document, { childList: true, subtree: true });
 });
