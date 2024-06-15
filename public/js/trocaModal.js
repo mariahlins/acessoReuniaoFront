1e83d0ede4ef06ee7d1c2588a0752b911914c61c
mariahlins <mariahlinssena4@gmail.com>
2024-06-15 18:08:31 -0300

    Alterações no 2 modal do cpf

--- public/js/trocaModal.js
+++ public/js/trocaModal.js
@@ -4,38 +4,54 @@ $('.modal').on('show.bs.modal', function () {
 });
 
 function updateModalContent(step) {
-    const currentStep = document.getElementById('step' + step);
-    const newHeader = currentStep.getAttribute('data-header');
-    document.getElementById('modalHeader').textContent = newHeader;
-  }
+  const currentStep = document.getElementById('step' + step);
+  const newHeader = currentStep.getAttribute('data-header');
+  document.getElementById('modalHeader').textContent = newHeader;
+}
 
-  //função de avançar
-  function nextStep(currentStep) {
-    document.getElementById('step' + currentStep).classList.remove('active');
-    document.getElementById('step' + (currentStep + 1)).classList.add('active');
-    updateModalContent(currentStep + 1);
-  }
+//função de avançar
+function nextStep(currentStep) {
+  document.getElementById('step' + currentStep).classList.remove('active');
+  document.getElementById('step' + (currentStep + 1)).classList.add('active');
+  updateModalContent(currentStep + 1);
+}
 
-  // função de voltar
-  function prevStep(currentStep) {
-    document.getElementById('step' + currentStep).classList.remove('active');
-    document.getElementById('step' + (currentStep - 1)).classList.add('active');
-    updateModalContent(currentStep - 1);
-  }
+// função de voltar
+function prevStep(currentStep) {
+  document.getElementById('step' + currentStep).classList.remove('active');
+  document.getElementById('step' + (currentStep - 1)).classList.add('active');
+  updateModalContent(currentStep - 1);
+}
 
-  // acaba o modal aqui
-  function finish() {
-    $('#modalDeCoworking').modal('hide');
-    resetModal();
-  }
+ // acaba o modal aqui
+function finish() {
+  $('#modalDeCoworking').modal('hide');
+  resetModal();
+}
 
-  function resetModal() {
-    document.querySelectorAll('.step').forEach((step) => step.classList.remove('active'));
-    document.getElementById('step1').classList.add('active');
-    updateModalContent(1);
-  }
+function resetModal() {
+  document.querySelectorAll('.step').forEach((step) => step.classList.remove('active'));
+  document.getElementById('step1').classList.add('active');
+  updateModalContent(1);
+}
 
-  // começa os conteudos do modal
-  $('#modalDeCoworking').on('show.bs.modal', function () {
-    resetModal();
+// começa os conteudos do modal
+$('#modalDeCoworking').on('show.bs.modal', function () {
+  resetModal();
+});
+
+
+//botoes
+function selectButton(button, group) {
+  const buttons = document.querySelectorAll(`.h5-item button`);
+  buttons.forEach(btn => {
+    if (btn !== button && btn.dataset.group === group) {
+      btn.classList.remove('btn-selected');
+    }
   });
+  button.classList.add('btn-selected');
+}
+
+
+
+// funções do dropdown de horas
