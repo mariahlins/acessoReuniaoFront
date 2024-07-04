550929e98e38df6a4a5cbb650e4e7bd4aa4341f8
Gustavo Santos <alexandregustavo00@gmail.com>
2024-07-03 21:37:56 -0300

    add o formatDate do commit anterior

--- public/js/API/conexaoApi.js
+++ public/js/API/conexaoApi.js
@@ -6,6 +6,13 @@ function formatarDataBr(dataEUA) {
     return `${dia}/${mes}/${ano}`;
 }
 
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
 function formatarDataEUA(dataBR) {
     const [dia, mes, ano] = dataBR.split('/');
     return `${ano}-${mes}-${dia}`;
